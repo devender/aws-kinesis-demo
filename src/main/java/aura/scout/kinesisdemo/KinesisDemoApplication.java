2c97c1e993858e3fedd24d7685c309f7c1f0771a
Devender Gollapally <dgollapally@insikt.com>
2019-11-19 09:20:59 -0800

    demo app

---
+++ src/main/java/aura/scout/kinesisdemo/KinesisDemoApplication.java
@@ -0,0 +1,44 @@
+package aura.scout.kinesisdemo;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@EnableBinding(Processor.class)
+@Slf4j
+public class KinesisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinesisDemoApplication.class, args);
+    }
+
+    @Bean
+    AWSCredentialsProvider awsCredentialsProvider(
+            @Value("${cloud.aws.credentials.access-key}") String awsAccessKey,
+            @Value("${cloud.aws.credentials.secret-key}") String awsSecret) {
+        return new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(awsAccessKey, awsSecret));
+    }
+
+    @Autowired
+    private Processor processor;
+
+    @StreamListener(Processor.INPUT)
+    public void consume(Map<String, Object> message) {
+        log.info("Received Message: {}", message);
+    }
+}
---
+++ src/test/java/aura/scout/kinesisdemo/KinesisDemoApplicationTests.java
@@ -0,0 +1,36 @@
+package aura.scout.kinesisdemo;
+
+
+import org.junit.Assert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+@SpringBootTest(classes = KinesisDemoApplication.class)
+class KinesisDemoApplicationTests {
+
+    @Autowired
+    private Processor processor;
+
+    @Test
+    void contextLoads() throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+            Map<String, Long> longMap = new HashMap<>(1);
+            longMap.put("Time", System.currentTimeMillis());
+            Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+            processor.output().send(MessageBuilder.withPayload(longMap).build());
+        }
+
+        Assert.assertTrue(true);
+    }
+
+}
